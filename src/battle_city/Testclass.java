0be5d2d0d00ba386b7ec14790b12f4640d521cc2
MPeti <petermuranyi94@gmail.com>
2018-04-20 18:22:30 +0200

    Game logic UML update

--- src/battle_city/Testclass.java
+++ src/battle_city/Testclass.java
@@ -1,5 +1,9 @@
 package battle_city;
 
+
+/**
+ * Check it out yo, javadock
+*/
 public class Testclass {
 	public static void main(String args[]) {
         System.out.println("Holly shit some fuckin' text");
